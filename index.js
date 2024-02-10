6768c6fce613b5588f1832b5f745db01e8dbec55
MuhammadAb98 <muhammadabdullah101998@gmail.com>
2024-02-10 16:19:17 +0500

    game

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
-// Function to perform arithmetic operations
-const performOperation = (num1, num2, operator) => {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var chalk_1 = require("chalk");
+var performOperation = function (num1, num2, operator) {
     switch (operator) {
         case "+":
             return num1 + num2;
@@ -15,63 +52,66 @@ const performOperation = (num1, num2, operator) => {
             throw new Error("Invalid operator");
     }
 };
-// Function to display result with colored output
-const displayResult = (result, operator) => {
-    let color = chalk.greenBright;
+var displayResult = function (result, operator) {
+    var color = chalk_1.default.greenBright;
     switch (operator) {
         case "+":
-            color = chalk.green;
+            color = chalk_1.default.green;
             break;
         case "-":
-            color = chalk.red;
+            color = chalk_1.default.red;
             break;
         case "*":
-            color = chalk.yellow;
+            color = chalk_1.default.yellow;
             break;
         case "/":
-            color = chalk.blue;
+            color = chalk_1.default.blue;
             break;
     }
-    console.log(color(`Result: ${result}`));
 };
-// Main calculator function
-const calculator = async () => {
-    const questions = [
-        {
-            type: "input",
-            name: "num1",
-            message: "Enter the first number:",
-            validate: (value) => !isNaN(Number(value)) || "Please enter a valid number",
-        },
-        {
-            type: "input",
-            name: "num2",
-            message: "Enter the second number:",
-            validate: (value) => !isNaN(Number(value)) || "Please enter a valid number",
-        },
-        {
-            type: "list",
-            name: "operator",
-            message: "Select an operation:",
-            choices: ["+", "-", "*", "/"],
-            // loop: true,
-        },
-    ];
-    try {
-        inquirer
-            .prompt(questions)
-            .then((answers) => {
-            const { num1, num2, operator } = answers;
-            // console.log("answer", answers);
-            const result = performOperation(parseFloat(num1), parseFloat(num2), operator);
-            displayResult(result, operator);
-            console.log(result);
-        })
-            .catch((err) => console.error(chalk.yellowBright("Error:", err.message)));
-    }
-    catch (error) {
-        console.error(chalk.red("Error:", error.message));
-    }
-};
-// Run the calculator
+var calculator = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var questions;
+    return __generator(this, function (_a) {
+        questions = [
+            {
+                type: "input",
+                name: "num1",
+                message: "Enter the first number:",
+                validate: function (value) {
+                    return !isNaN(Number(value)) || "Please enter a valid number";
+                },
+            },
+            {
+                type: "input",
+                name: "num2",
+                message: "Enter the second number:",
+                validate: function (value) {
+                    return !isNaN(Number(value)) || "Please enter a valid number";
+                },
+            },
+            {
+                type: "list",
+                name: "operator",
+                message: "Select an operation:",
+                choices: ["+", "-", "*", "/"],
+            },
+        ];
+        try {
+            inquirer_1.default
+                .prompt(questions)
+                .then(function (answers) {
+                var num1 = answers.num1, num2 = answers.num2, operator = answers.operator;
+                // console.log("answer", answers);
+                var result = performOperation(parseFloat(num1), parseFloat(num2), operator);
+                displayResult(result, operator);
+                console.log(result);
+            })
+                .catch(function (err) { return console.error(chalk_1.default.yellowBright("Error:", err.message)); });
+        }
+        catch (error) {
+            console.error(chalk_1.default.red("Error:", error.message));
+        }
+        return [2 /*return*/];
+    });
+}); };
 calculator();
